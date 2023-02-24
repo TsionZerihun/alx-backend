8d8de34ab94bbe6fb60197a3e9c71285bc6afc03
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-24 20:35:32 +0300

    fixing py code

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+import the parent BaseCaching class
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """caching using FIFO algorithm"""
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        if (key is None) or (item is None):
+            pass
+        elif len(self.cache_data) < BaseCaching.MAX_ITEMS:
+            self.cache_data[key] = item
+        else:
+            print("DISCARD: {}".format(list(self.cache_data)[0]))
+            self.cache_data.pop(list(self.cache_data)[0])
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Must return the value in self.cache_data
+        """
+        return (self.cache_data.get(key))
