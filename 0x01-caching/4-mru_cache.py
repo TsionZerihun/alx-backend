d50ce2d17ae69b85c83ef0081c2616add00fa994
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-27 08:53:38 +0300

    fix error

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -15,13 +15,13 @@ class MRUCache(BaseCaching):
         Add an item to the cache
         """
         if key is not None and item is not None:
-            if key in self.cache_data:
+            if key not in self.cache_data:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    discarded = self.cache_data.popitem()[0]
+                    print("DISCARD: {}".format(discarded))
+            else:
                 del self.cache_data[key]
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_key = next(iter(self.cache_data))
-                del self.cache_data[first_key]
-                print("DISCARD: {}".format(first_key))
 
     def get(self, key):
         """
